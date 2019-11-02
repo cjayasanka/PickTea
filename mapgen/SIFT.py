bfaced0ceb3e2847832452480b72d070eb915dc8
cjayasanka <cjayasanka93@gmail.com>
2019-11-02 14:13:27 +0530

    Initial commit

---
+++ mapgen/SIFT.py
@@ -0,0 +1,8 @@
+# steps of sift process
+# 1.Scale-Space Extrema Detection
+# 2.Keypoint Localization
+# 3.Orientation Assignment
+# 4.Local Descriptor Creation
+
+if __name__ == "__main__":
+    print("Hello")
