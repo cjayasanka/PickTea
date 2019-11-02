7e030875a38a555d56aeeca3dad729ff2ace2f00
cjayasanka <cjayasanka93@gmail.com>
2019-11-02 20:39:24 +0530

    adding keypoint candidate finder and helper funct

--- mapgen/SIFT.py
+++ mapgen/SIFT.py
@@ -7,6 +7,14 @@
 from PIL import Image
 from scipy import ndimage
 import math
+import numpy as np
+
+
+def g_filter(sigma):
+    window = 2 * np.ceil(3 * sigma) + 1
+    x, y = np.mgrid[-window // 2 + 1:window // 2 + 1, -window // 2 + 1:window // 2 + 1]
+    top = np.exp(-((x**2 + y**2) / (2.0 * sigma**2))) / (2 * np.pi * sigma**2)
+    return top / top.sum()
 
 
 # 1st step of sift
@@ -37,6 +45,110 @@ def compute_dog(scale_space):
             dog_in_octave.append(scale_space[i][j] - scale_space[i][j+1])
         dog.append(dog_in_octave)
     return dog
+
+
+# extrema detection and keypoints detection
+def find_keypoints(dog):
+    keypoints = []
+    # extrema = []
+    for scale in dog:
+        scale_extrema = []
+        for s in range(2):
+            img_size = scale[0].size
+            # blank = Image.fromarray(np.zeros(scale[0].size, dtype=np.uint8))
+            for i in range(img_size[0]):
+                for j in range(img_size[1]):
+                    if is_min_max(scale, s+1, i, j, img_size):
+                        # blank[i, j] = scale[s+1][i, j]
+                        scale_extrema.append([i, j, s+1])
+
+            # scale_extrema.append(blank)
+
+        # extrema.append(scale_extrema)
+        scale_keypoints = subpixel(scale_extrema, scale)
+        keypoints.append(scale_keypoints)
+
+    return keypoints
+
+
+def is_min_max(scale, img_no, i, j, img_size):
+    value = scale[img_no][i, j]
+    high = False
+    low = False
+    if value > scale[img_no-1][i, j]:
+        # low scale is lower
+        high = True
+    elif value < scale[img_no-1][i, j]:
+        # low scale is higher
+        low = True
+    else:
+        return False
+
+    if high:
+        if value < scale[img_no+1][i, j]:
+            # high scale is higher - not max or min
+            return False
+        # same scale level
+        if value < scale[img_no][i+1, j] or value < scale[img_no][i-1, j]:
+            return False
+        if value < scale[img_no][i, j+1] or value < scale[img_no][i, j-1]:
+            return False
+        if value < scale[img_no][i+1, j+1] or value < scale[img_no][i+1, j-1]:
+            return False
+        if value < scale[img_no][i-1, j+1] or value < scale[img_no][i-1, j-1]:
+            return False
+        # one scale lower
+        if value < scale[img_no-1][i + 1, j] or value < scale[img_no-1][i - 1, j]:
+            return False
+        if value < scale[img_no-1][i, j + 1] or value < scale[img_no-1][i, j - 1]:
+            return False
+        if value < scale[img_no-1][i + 1, j + 1] or value < scale[img_no-1][i + 1, j - 1]:
+            return False
+        if value < scale[img_no-1][i - 1, j + 1] or value < scale[img_no-1][i - 1, j - 1]:
+            return False
+        # one scale higher
+        if value < scale[img_no + 1][i + 1, j] or value < scale[img_no + 1][i - 1, j]:
+            return False
+        if value < scale[img_no + 1][i, j + 1] or value < scale[img_no + 1][i, j - 1]:
+            return False
+        if value < scale[img_no + 1][i + 1, j + 1] or value < scale[img_no + 1][i + 1, j - 1]:
+            return False
+        if value < scale[img_no + 1][i - 1, j + 1] or value < scale[img_no + 1][i - 1, j - 1]:
+            return False
+
+    if low:
+        if value > scale[img_no+1][i, j]:
+            # high scale is lower - not max or min
+            return False
+        # same scale level
+        if value > scale[img_no][i + 1, j] or value > scale[img_no][i - 1, j]:
+            return False
+        if value > scale[img_no][i, j + 1] or value > scale[img_no][i, j - 1]:
+            return False
+        if value > scale[img_no][i + 1, j + 1] or value > scale[img_no][i + 1, j - 1]:
+            return False
+        if value > scale[img_no][i - 1, j + 1] or value > scale[img_no][i - 1, j - 1]:
+            return False
+        # one scale lower
+        if value > scale[img_no - 1][i + 1, j] or value > scale[img_no - 1][i - 1, j]:
+            return False
+        if value > scale[img_no - 1][i, j + 1] or value > scale[img_no - 1][i, j - 1]:
+            return False
+        if value > scale[img_no - 1][i + 1, j + 1] or value > scale[img_no - 1][i + 1, j - 1]:
+            return False
+        if value > scale[img_no - 1][i - 1, j + 1] or value > scale[img_no - 1][i - 1, j - 1]:
+            return False
+        # one scale higher
+        if value > scale[img_no + 1][i + 1, j] or value > scale[img_no + 1][i - 1, j]:
+            return False
+        if value > scale[img_no + 1][i, j + 1] or value > scale[img_no + 1][i, j - 1]:
+            return False
+        if value > scale[img_no + 1][i + 1, j + 1] or value > scale[img_no + 1][i + 1, j - 1]:
+            return False
+        if value > scale[img_no + 1][i - 1, j + 1] or value > scale[img_no + 1][i - 1, j - 1]:
+            return False
+
+    return True
 
 
 if __name__ == "__main__":
