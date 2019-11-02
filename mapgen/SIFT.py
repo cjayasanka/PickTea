6e7205a8733d1f6823ca5f89e9f0b9fa1d310206
cjayasanka <cjayasanka93@gmail.com>
2019-11-02 14:17:01 +0530

    data and file open

--- mapgen/SIFT.py
+++ mapgen/SIFT.py
@@ -4,5 +4,11 @@
 # 3.Orientation Assignment
 # 4.Local Descriptor Creation
 
+from PIL import Image
+
 if __name__ == "__main__":
     print("Hello")
+    # open image
+    img = Image.open('data/field.jpg').convert('LA')
+    img_size = img.size
+    print(img_size)
